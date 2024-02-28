121daf6feb45beb971b04414b44bfa3ce70594e7
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2024-02-28 12:08:26 -0300

    Database and Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        This is a screen for authenticated users only
+      </div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
   );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://above-kingfish-28.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
