8873a8bbeca1043e6f492028687671210fd5a0cf
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2024-03-11 19:03:20 -0300

    Workspaces Sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,10 +4,12 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 
+import { MobileSidebar } from "./mobile-sidebar";
+
 export const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/* TODO: Mobile Sidebar */}
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
