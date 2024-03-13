dae4d538187234b152bcbc6ea8356c8eee0830d8
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2024-03-13 00:12:42 -0300

    Board Page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,10 +1,17 @@
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
+
 import { OrgControl } from "./_components/org-control";
 
-const OrganizationIdLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <OrgControl />
